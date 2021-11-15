5780699e174022a4941ca8b4a3a93ad286c870bd
Synx-x <siyandamtamo@gmail.com>
2021-11-15 20:43:45 +0200

    changed eslint, made nav

---
+++ components/navBar.tsx
@@ -0,0 +1,42 @@
+import React, { useState, useMemo } from "react";
+import * as styles from "../styles/components/Navbar.module.scss";
+
+const NavBar = React.memo((props) => {
+  const [count, useCount] = useState(0);
+
+  return (
+    <nav key={count} className={styles.nav}>
+      <ul className={styles.navUl}>{props.children}</ul>
+    </nav>
+  );
+});
+
+export const NavItems = React.memo((props) => {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <li className={styles.navItem}>
+      <a href="#" className={styles.iconButton} onClick={() => setOpen(!open)}>
+        {props.icon}
+      </a>
+      {open && props.children}
+    </li>
+  );
+});
+
+export const DropdownMenu = React.memo((props) => {
+  const [activeMenu, setActiveMenu] = useState("main");
+
+  return <div className={styles.dropdownMenu}></div>;
+});
+
+export const DropdownItem = React.memo((props) => {
+  return (
+    <a href="#" className={styles.menuItem}>
+      <span className={styles.iconLeft}>{props.leftIcon}</span>
+      {props.children}
+    </a>
+  );
+});
+
+export default NavBar;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,33 +3,44 @@ import "../styles/vendors/_cssReset.scss";
 import "../styles/vendors/_normalize.scss";
 import { motion, AnimatePresence } from "framer-motion";
 import type { AppProps } from "next/app";
+import Navbar, {
+  DropdownItem,
+  DropdownMenu,
+  NavItems,
+} from "../components/navBar";
+import React from "react";
 
 function App({ Component, pageProps, router }: AppProps) {
-  // const variants = {
-  //   hidden: { opacity: 0, x: -200, y: 0 },
-  //   enter: { opacity: 1, x: 0, y: 0 },
-  //   exit: { opacity: 0, x: 0, y: -100 },
-  // };
-  // variants={variants} // Pass the variant object into Framer Motion
-  // initial="hidden" // Set the initial state to variants.hidden
-  // animate="enter" // Animated state to variants.enter
-  // exit="exit" // Exit state (used later) to variants.exit
-  // transition={{ type: 'linear' }} // Set the transition to linear
-
   return (
-    <AnimatePresence>
-      <motion.div
-        key={router.route}
-        initial="pageInitial"
-        animate="pageAnimate"
-        variants={{
-          pageInitial: { opacity: 0 },
-          pageAnimate: { opacity: 1 },
-        }}
-      >
-        <Component {...pageProps} />
-      </motion.div>
-    </AnimatePresence>
+    <>
+      <Navbar>
+        <NavItems icon="🤣" />
+        <NavItems icon="🤣" />
+        <NavItems icon="🐤" />
+        <NavItems icon="🤣" />
+
+        <NavItems icon="😃">
+          <DropdownMenu>
+            <DropdownItem>Settings</DropdownItem>
+            <DropdownItem>Profile</DropdownItem>
+          </DropdownMenu>
+        </NavItems>
+      </Navbar>
+
+      <AnimatePresence>
+        <motion.div
+          key={router.route}
+          initial="pageInitial"
+          animate="pageAnimate"
+          variants={{
+            pageInitial: { opacity: 0 },
+            pageAnimate: { opacity: 1 },
+          }}
+        >
+          <Component {...pageProps} />
+        </motion.div>
+      </AnimatePresence>
+    </>
   );
 }
 
